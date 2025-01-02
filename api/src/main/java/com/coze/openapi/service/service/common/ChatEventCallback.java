1673e6f16c865738164d2d27c9f0fbbbbe575ce5
gouqinggan <gouqinggan@bytedance.com>
2025-01-02 17:02:18 +0800

    save
    Change-Id: I4f982004382ff0eca5697f26444712144d5f9e2b

--- api/src/main/java/com/coze/openapi/service/service/common/ChatEventCallback.java
+++ api/src/main/java/com/coze/openapi/service/service/common/ChatEventCallback.java
@@ -11,25 +11,26 @@ import io.reactivex.FlowableEmitter;
 
 public class ChatEventCallback extends AbstractEventCallback<ChatEvent> {
 
-    public ChatEventCallback(FlowableEmitter<ChatEvent> emitter) {
-        super(emitter);
+  public ChatEventCallback(FlowableEmitter<ChatEvent> emitter) {
+    super(emitter);
+  }
+
+  @Override
+  protected boolean processLine(String line, BufferedReader reader, String logID)
+      throws IOException {
+    if (line.startsWith("event:")) {
+      String event = line.substring(6).trim();
+      String data = reader.readLine().substring(5).trim();
+
+      Map<String, String> eventLine = new HashMap<>();
+      eventLine.put("event", event);
+      eventLine.put("data", data);
+
+      ChatEvent eventData = ChatEvent.parseEvent(eventLine, logID);
+
+      emitter.onNext(eventData);
+      return eventData.isDone();
     }
-
-    @Override
-    protected boolean processLine(String line, BufferedReader reader, String logID) throws IOException {
-        if (line.startsWith("event:")) {
-            String event = line.substring(6).trim();
-            String data = reader.readLine().substring(5).trim();
-
-            Map<String, String> eventLine = new HashMap<>();
-            eventLine.put("event", event);
-            eventLine.put("data", data);
-
-            ChatEvent eventData = ChatEvent.parseEvent(eventLine, logID);
-
-            emitter.onNext(eventData);
-            return eventData.isDone();
-        }
-        return false;
-    }
+    return false;
+  }
 }
