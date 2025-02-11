9c4b2c103a278c73352fadca8ef7cd596080a00c
hanzeINGH <gouqinggan@bytedance.com>
2025-02-11 14:27:39 +0800

    format code
    Change-Id: Ic7a995b5cb4a51b61e7fa9afb4b08b9e1013befc

--- example/src/main/java/example/auth/ExampleJWTBuilder.java
+++ example/src/main/java/example/auth/ExampleJWTBuilder.java
@@ -1,21 +1,21 @@
 package example.auth;
 
 import java.security.PrivateKey;
-//import java.util.Date;
 import java.util.Map;
 
 import com.coze.openapi.service.auth.JWTBuilder;
-//import com.coze.openapi.service.utils.Utils;
-//
-//import io.jsonwebtoken.JwtBuilder;
-//import io.jsonwebtoken.Jwts;
+
 import lombok.NoArgsConstructor;
+
+// import com.coze.openapi.service.utils.Utils;
+// import io.jsonwebtoken.JwtBuilder;
+// import io.jsonwebtoken.Jwts;
+// import java.util.Date;
 
 /**
  * This is an example of a JWT builder implementation. If you are using jjwt version 0.12.x or
- * above, please refer to this example to implement your own JWT parser.
- * 
- * When you try to run this code, please uncomment the commented code
+ * above, please refer to this example to implement your own JWT parser. When you try to run this
+ * code, please uncomment the commented code and imports
  */
 @NoArgsConstructor
 public class ExampleJWTBuilder implements JWTBuilder {
@@ -38,23 +38,23 @@ public class ExampleJWTBuilder implements JWTBuilder {
     try {
       long now = System.currentTimeMillis() / 1000;
 
-//      JwtBuilder jwtBuilder =
-//          Jwts.builder()
-//              .header()
-//              .add(header)
-//              .and()
-//              .issuer(this.clientID)
-//              .audience()
-//              .add(this.hostName)
-//              .and()
-//              .issuedAt(new Date(now * 1000))
-//              .expiration(new Date((now + ttl) * 1000))
-//              .id(Utils.genRandomSign(16))
-//              .signWith(privateKey, Jwts.SIG.RS256);
-//      if (sessionName != null) {
-//        jwtBuilder.claim("session_name", sessionName);
-//      }
-//      return jwtBuilder.compact();
+      //      JwtBuilder jwtBuilder =
+      //          Jwts.builder()
+      //              .header()
+      //              .add(header)
+      //              .and()
+      //              .issuer(this.clientID)
+      //              .audience()
+      //              .add(this.hostName)
+      //              .and()
+      //              .issuedAt(new Date(now * 1000))
+      //              .expiration(new Date((now + ttl) * 1000))
+      //              .id(Utils.genRandomSign(16))
+      //              .signWith(privateKey, Jwts.SIG.RS256);
+      //      if (sessionName != null) {
+      //        jwtBuilder.claim("session_name", sessionName);
+      //      }
+      //      return jwtBuilder.compact();
 
     } catch (Exception e) {
       throw new RuntimeException("Failed to generate JWT", e);
