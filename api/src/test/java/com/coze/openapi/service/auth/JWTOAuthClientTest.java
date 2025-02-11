bbc8f0f95f95f688c89f16c327843d706a40a067
hanzeINGH <gouqinggan@bytedance.com>
2025-02-11 14:01:41 +0800

    add ut
    Change-Id: Iedee8fcc0f085e22f9ebe1e3bbf6d27428d5a65c

--- api/src/test/java/com/coze/openapi/service/auth/JWTOAuthClientTest.java
+++ api/src/test/java/com/coze/openapi/service/auth/JWTOAuthClientTest.java
@@ -64,6 +64,7 @@ class JWTOAuthClientTest {
             .publicKey("test_public_key")
             .privateKey(TEST_PRIVATE_KEY)
             .ttl(900)
+            .jwtBuilder(new DefaultJWTBuilder("test_client_id", "api.coze.cn"))
             .build();
     TestUtils.setField(client, "api", mockApi);
   }
