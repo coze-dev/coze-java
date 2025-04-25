4c731410e768252cc8d4c7b420568f02dbd6ab63
hanzeINGH <gouqinggan@bytedance.com>
2025-04-25 15:22:43 +0800

    fix ut
    Change-Id: Ia9baf1ec8dcfee84856adb5ca2cb5e7096f42179

--- api/src/main/java/com/coze/openapi/client/auth/GetJWTAccessTokenReq.java
+++ api/src/main/java/com/coze/openapi/client/auth/GetJWTAccessTokenReq.java
@@ -3,11 +3,15 @@ package com.coze.openapi.client.auth;
 import com.coze.openapi.client.auth.model.SessionContext;
 import com.coze.openapi.client.auth.scope.Scope;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetJWTAccessTokenReq {
   private Integer ttl;
   private Scope scope;
