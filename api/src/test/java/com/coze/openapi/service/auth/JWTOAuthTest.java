7407f4fa842c4a99ba1955184f1f6fede4aa057e
hanzeINGH <gouqinggan@bytedance.com>
2025-04-25 15:04:17 +0800

    fix ut
    Change-Id: Ifa8b905365d3444e183494230b18a547d8e65e26

--- api/src/test/java/com/coze/openapi/service/auth/JWTOAuthTest.java
+++ api/src/test/java/com/coze/openapi/service/auth/JWTOAuthTest.java
@@ -2,15 +2,14 @@ package com.coze.openapi.service.auth;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-import com.coze.openapi.client.auth.GetJWTAccessTokenReq;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.coze.openapi.client.auth.GetJWTAccessTokenReq;
 import com.coze.openapi.client.auth.OAuthToken;
 
 @ExtendWith(MockitoExtension.class)
