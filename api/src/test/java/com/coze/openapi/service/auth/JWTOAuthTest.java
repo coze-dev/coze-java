ede81c7c8393346b984bbabf4e5724ea97917cbc
hanzeINGH <gouqinggan@bytedance.com>
2025-04-25 14:57:04 +0800

    fix ut
    Change-Id: Iacacef845a5818159c872f7b0891c5e8decbef0e

--- api/src/test/java/com/coze/openapi/service/auth/JWTOAuthTest.java
+++ api/src/test/java/com/coze/openapi/service/auth/JWTOAuthTest.java
@@ -5,6 +5,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
+import com.coze.openapi.client.auth.GetJWTAccessTokenReq;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -37,13 +38,13 @@ class JWTOAuthTest {
   void testTokenFirstTime() {
     when(jwtClient.getTtl()).thenReturn(600);
     when(oAuthToken.getAccessToken()).thenReturn("test-token");
-    when(jwtClient.getAccessToken((Integer) any(), any(), any())).thenReturn(oAuthToken);
+    when(jwtClient.getAccessToken((GetJWTAccessTokenReq) any())).thenReturn(oAuthToken);
 
     jwtOAuth = new JWTOAuth(jwtClient);
     String token = jwtOAuth.token();
 
     assertEquals("test-token", token);
-    verify(jwtClient, times(1)).getAccessToken((Integer) any(), any(), any());
+    verify(jwtClient, times(1)).getAccessToken((GetJWTAccessTokenReq) any());
   }
 
   @Test
@@ -51,7 +52,7 @@ class JWTOAuthTest {
     when(jwtClient.getTtl()).thenReturn(600);
     when(oAuthToken.getAccessToken()).thenReturn("test-token");
     when(oAuthToken.getExpiresIn()).thenReturn(600);
-    when(jwtClient.getAccessToken((Integer) any(), any(), any())).thenReturn(oAuthToken);
+    when(jwtClient.getAccessToken((GetJWTAccessTokenReq) any())).thenReturn(oAuthToken);
 
     jwtOAuth = new JWTOAuth(jwtClient);
 
@@ -59,7 +60,7 @@ class JWTOAuthTest {
     String token2 = jwtOAuth.token();
 
     assertEquals(token1, token2);
-    verify(jwtClient, times(2)).getAccessToken((Integer) any(), any(), any());
+    verify(jwtClient, times(2)).getAccessToken((GetJWTAccessTokenReq) any());
   }
 
   @Test
@@ -68,21 +69,21 @@ class JWTOAuthTest {
     String sessionName = "test-session";
 
     when(oAuthToken.getAccessToken()).thenReturn("test-token");
-    when(jwtClient.getAccessToken(eq(ttl), any(), eq(sessionName))).thenReturn(oAuthToken);
+    when(jwtClient.getAccessToken((GetJWTAccessTokenReq) any())).thenReturn(oAuthToken);
 
     jwtOAuth = JWTOAuth.builder().jwtClient(jwtClient).ttl(ttl).sessionName(sessionName).build();
 
     String token = jwtOAuth.token();
 
     assertEquals("test-token", token);
-    verify(jwtClient).getAccessToken(ttl, null, sessionName);
+    verify(jwtClient).getAccessToken((GetJWTAccessTokenReq) any());
   }
 
   @Test
   void testTokenRefresh() throws InterruptedException {
     when(oAuthToken.getAccessToken()).thenReturn("test-token");
     when(oAuthToken.getExpiresIn()).thenReturn(2);
-    when(jwtClient.getAccessToken((Integer) any(), any(), any())).thenReturn(oAuthToken);
+    when(jwtClient.getAccessToken((GetJWTAccessTokenReq) any())).thenReturn(oAuthToken);
 
     jwtOAuth = JWTOAuth.builder().jwtClient(jwtClient).ttl(2).build();
 
@@ -91,29 +92,6 @@ class JWTOAuthTest {
     String token2 = jwtOAuth.token();
 
     assertEquals("test-token", token2);
-    verify(jwtClient, times(2)).getAccessToken((Integer) any(), any(), any());
-  }
-
-  @Test
-  void testGetRefreshBeforeWithDifferentTtls() {
-    when(oAuthToken.getAccessToken()).thenReturn("test-token");
-
-    // Test ttl >= 600
-    jwtOAuth = JWTOAuth.builder().jwtClient(jwtClient).ttl(600).build();
-    when(jwtClient.getAccessToken((Integer) any(), any(), any())).thenReturn(oAuthToken);
-    jwtOAuth.token();
-    verify(jwtClient).getAccessToken(eq(600), any(), any());
-
-    // Test 60 <= ttl < 600
-    jwtOAuth = JWTOAuth.builder().jwtClient(jwtClient).ttl(100).build();
-    when(jwtClient.getAccessToken((Integer) any(), any(), any())).thenReturn(oAuthToken);
-    jwtOAuth.token();
-    verify(jwtClient).getAccessToken(eq(100), any(), any());
-
-    // Test 30 <= ttl < 60
-    jwtOAuth = JWTOAuth.builder().jwtClient(jwtClient).ttl(30).build();
-    when(jwtClient.getAccessToken((Integer) any(), any(), any())).thenReturn(oAuthToken);
-    jwtOAuth.token();
-    verify(jwtClient).getAccessToken(eq(30), any(), any());
+    verify(jwtClient, times(2)).getAccessToken((GetJWTAccessTokenReq) any());
   }
 }
