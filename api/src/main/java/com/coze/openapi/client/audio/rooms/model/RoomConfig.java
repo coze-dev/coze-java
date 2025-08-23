4de009a3fcff22202e9a23e6d0e77e8389852bef
xiangshicheng <1992514xsc@ml>
2025-08-23 11:10:24 +0800

    同声传译在RoomConfig内增加翻译语言类型属性

--- api/src/main/java/com/coze/openapi/client/audio/rooms/model/RoomConfig.java
+++ api/src/main/java/com/coze/openapi/client/audio/rooms/model/RoomConfig.java
@@ -18,4 +18,45 @@ public class RoomConfig {
   public static RoomConfig of(AudioCodec codec) {
     return RoomConfig.builder().audioConfig(RoomAudioConfig.builder().codec(codec).build()).build();
   }
+
+  private String room_mode = "";
+
+  private TranslateConfig translate_config;
+
+  class TranslateConfig {
+    private String from = "";
+    private String to = "";
+
+    public String getFrom() {
+      return from;
+    }
+
+    public void setFrom(String from) {
+      this.from = from;
+    }
+
+    public String getTo() {
+      return to;
+    }
+
+    public void setTo(String to) {
+      this.to = to;
+    }
+  }
+
+  public String getRoom_mode() {
+    return room_mode;
+  }
+
+  public void setRoom_mode(String room_mode) {
+    this.room_mode = room_mode;
+  }
+
+  public TranslateConfig getTranslate_config() {
+    return translate_config;
+  }
+
+  public void setTranslate_config(TranslateConfig translate_config) {
+    this.translate_config = translate_config;
+  }
 }
